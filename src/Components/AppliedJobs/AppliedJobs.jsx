af8f00b4e620c40021ac61209aa5346b701dc220
naeem <naeemislamjr182@gmail.com>
2023-09-23 05:10:29 +0600

    set home and footer basic layout

---
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,11 @@
+
+
+const AppliedJobs = () => {
+    return (
+        <div>
+            <h2>job iiiiiii applied</h2>
+        </div>
+    )
+}
+
+export default AppliedJobs
--- src/main.jsx
+++ src/main.jsx
@@ -6,16 +6,35 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 import Home from './Components/Home/Home'
 import Root from './Components/Root/Root'
+import AppliedJobs from './Components/AppliedJobs/AppliedJobs'
+import ErrorPage from './ErrorPage/ErrorPage'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
         element: <Home></Home>
-      }
+      },
+      {
+        path: '/jobs',
+        element: <Home></Home>
+      },
+      {
+        path: '/applied',
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        path: '/statistics',
+        element: <Home></Home>
+      },
+      {
+        path: '/blogs',
+        element: <Home></Home>
+      },
     ]
   },
 
